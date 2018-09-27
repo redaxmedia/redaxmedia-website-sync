8a0350eb3d41eff7395777a0bb7df37fa4d992fb
redaxmedia <info@redaxmedia.com>
2018-09-27 22:25:08 +0200

    Use latest grunt ftpress

--- tasks/ftpress.js
+++ tasks/ftpress.js
@@ -14,7 +14,12 @@ module.exports = grunt =>
 			options:
 			{
 				url: process.env.FTP_URL,
-				command: 'mirror {SOURCE} {TARGET} --reverse --delete-first --parallel=10 --use-pget-n=10; chmod 0444 www/htdocs/w00976cb/redaxmedia/config.php; exit'
+				command:
+				[
+					'mirror {SOURCE} {TARGET} --reverse --delete-first --parallel=10 --use-pget-n=10',
+					'chmod 0444 {TARGET}/config.php',
+					'exit'
+				]
 			}
 		}
 	};
